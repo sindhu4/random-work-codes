e0f5d9542ec84046dd27cbdd18b8ee1cfdf7d29a
sindhu4 <sindhu.4488@gmail.com>
2017-01-09 19:42:03 -0600

    Update down_across.R

--- down_across.R
+++ down_across.R
@@ -1,23 +1,24 @@
-# Match row IDs in one file with Column IDs in another files to extract data pertaining only to file with required Column IDs. 
-# Matching row with column instead of transposing data to match row with row.
-# read in the index(Down) and the raw data(Across w/out transpose nor first column name)
+# Match row IDs in one file with Column IDs in another file to extract data pertaining only to files with required Column IDs. 
+# Due to data files of extreme size, match rows with columns without transposing data to avoid data loss.
+
+# read in the index(Down) and the raw data(Across)
 Down <-read.delim("Down.csv",sep=",")
 
 # header=FALSE prevents the loss or change of data.
 Across <-read.delim("Across.csv", header=FALSE, sep=",")
 
 # coerce to vectors
-newAcross <- as.vector(Across)
 newDown <- as.vector(Down)
+newAcross <- as.vector(Across)
 
-# create a boolean table that finds what is wanted.
-tryingData <- (Across[1,] %in% Down$StudyID)
+# create a boolean table that finds what is wanted(TRUE indicates wanted).
+testData <- (Across[1,] %in% Down$StudyID)
 
 # extract just the wanted from all data.
-reducedBool <- Across[,tryingData]
+reducedbool <- Across[,testData]
 
 # replace the Vn column names with nulls.
-c1 = unname(reducedBool)
+reducedData = unname(reducedbool)
 
 #save reduced file
-write.csv(c1, file = "cl.csv")
+write.csv(reducedData, file = "reducedData.csv")
